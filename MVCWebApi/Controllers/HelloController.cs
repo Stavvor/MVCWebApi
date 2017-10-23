04475e35a5bd13661b91f1c6ed0c0a7c03258053
Stavvor <staworowskia@gmail.com>
2017-10-23 20:47:59 +0200

    added HelloController

---
+++ MVCWebApi/Controllers/HelloController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVCWebApi.Services;
+
+namespace MVCWebApi.Services
+{
+    [Route("[controller]")]
+    public class HelloController : Controller
+    {
+        HelloService _helloService;
+
+        public HelloController()
+        {
+            _helloService = new HelloService();
+        }
+
+        //// GET api/values
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value2" };
+        //}
+
+        // GET api/values/6
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return _helloService.helloWorld();
+        }
+
+        //// POST api/values
+        //[HttpPost]
+        //public void Post([FromBody]string value)
+        //{
+        //}
+
+        //// PUT api/values/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE api/values/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ MVCWebApi/Services/HelloService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCWebApi.Services
+{
+    public class HelloService
+    {
+        public string helloWorld()
+        {
+            return "Hello world!";
+        }
+    }
+}
