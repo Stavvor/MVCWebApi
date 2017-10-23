f2ae55a5a6aeab7234e05c9119e47d90360eefaa
Stavvor <staworowskia@gmail.com>
2017-10-23 21:00:24 +0200

    added entity framework nuget package

--- MVCWebApi/Controllers/HelloController.cs
+++ MVCWebApi/Controllers/HelloController.cs
@@ -17,36 +17,11 @@ namespace MVCWebApi.Services
             _helloService = new HelloService();
         }
 
-        //// GET api/values
-        //[HttpGet]
-        //public IEnumerable<string> Get()
-        //{
-        //    return new string[] { "value2" };
-        //}
-
         // GET api/values/6
         [HttpGet("{id}")]
         public string Get(int id)
         {
             return _helloService.helloWorld();
         }
-
-        //// POST api/values
-        //[HttpPost]
-        //public void Post([FromBody]string value)
-        //{
-        //}
-
-        //// PUT api/values/5
-        //[HttpPut("{id}")]
-        //public void Put(int id, [FromBody]string value)
-        //{
-        //}
-
-        //// DELETE api/values/5
-        //[HttpDelete("{id}")]
-        //public void Delete(int id)
-        //{
-        //}
     }
 }
