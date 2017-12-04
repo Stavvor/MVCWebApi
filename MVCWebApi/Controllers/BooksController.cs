56e39cec9a8274924424c3c3b9264466a6eae89f
Stavvor <staworowskia@gmail.com>
2017-12-04 19:33:17 +0100

    deployment

--- MVCWebApi/Controllers/BooksController.cs
+++ MVCWebApi/Controllers/BooksController.cs
@@ -47,7 +47,7 @@ namespace MVCWebApi.Controllers
         // POST: Books/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost("Books/Createe")]
+        [HttpPost("Books/Create")]
         public async Task<IActionResult> Create([FromBody] Book book)
         {
             if (ModelState.IsValid)
